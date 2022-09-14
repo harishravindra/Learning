a8784c3e91501715d68f984663648e19140b3b66
Harish Babu03 <Harish_Babu03@ad.Infosys.com>
2022-09-14 14:52:39 +0530

    vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,40 @@
+######### create  a vpc ########
+
+resource "aws_vpc" "MY_VPC" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+}
+
+resource "aws_instance" "server" {
+  count = 4 
+  ami           = "ami-a1b2c3d4"
+  instance_type = "t2.micro"
+   vpc_id      = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Server ${count.index}"
+  }
+}
+
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow  http inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "http from VPC"
+    from_port        =  80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }s
